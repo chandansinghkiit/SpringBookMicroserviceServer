ae831b7e9d6a7f18d176ebe2fc653fce51295516
chandansinghkiit <7cksingh@gmail.com>
2019-04-19 11:49:41 +0530

    minor changes for crossOrigin

--- src/main/java/com/mystyle/book/repository/BookRepository.java
+++ src/main/java/com/mystyle/book/repository/BookRepository.java
@@ -2,10 +2,12 @@ package com.mystyle.book.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.mystyle.book.domain.Book;
 
 @RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
